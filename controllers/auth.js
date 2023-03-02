5ca474303898010ec38a5774acef2e8cf61316eb
JoelEncinas <joelencinasmartin@gmail.com>
2023-03-02 17:29:47 +0100

    generate jwt token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,5 @@
 const User = require("../models/User");
 const ErrorResponse = require("../utils/errorResponse");
-const errorResponse = require("../utils/errorResponse");
 
 exports.register = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -12,10 +11,7 @@ exports.register = async (req, res, next) => {
       password,
     });
 
-    res.status(201).json({
-      success: true,
-      user,
-    });
+    sendToken(user, 201, res);
   } catch (error) {
     next(error);
   }
@@ -41,10 +37,7 @@ exports.login = async (req, res, next) => {
       return next(new ErrorResponse("Invalid Credentials", 401));
     }
 
-    res.status(200).json({
-      success: true,
-      token: "lmao",
-    });
+    sendToken(user, 200, res);
   } catch (error) {
     res.status(500).json({ success: false, error: error.message });
   }
@@ -56,4 +49,9 @@ exports.forgotpassword = (req, res, next) => {
 
 exports.resetpassword = (req, res, next) => {
   res.send("Reset password route");
+};
+
+const sendToken = (user, statusCode, res) => {
+  const token = user.getSignedToken();
+  res.status(statusCode).json({ success: true, token });
 };

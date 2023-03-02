482ea57c9efa0ed6823a903786d5231d0edbc0c9
JoelEncinas <joelencinasmartin@gmail.com>
2023-03-02 02:10:00 +0100

    added userschema and mongodb connection

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-require("dotenv").config({ path: "./config.env" });
+require("dotenv").config({ path: "./config.env" }); // needs to be at the top
 const express = require("express");
+const connectDB = require("./config/db");
+
+// connect to db
+connectDB();
 
 const app = express();
 
@@ -9,4 +13,12 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
+
+// easier to read mongoDB connection err handler
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Logged Error: ${err}`);
+  server.close(() => process.exit(1));
+});
